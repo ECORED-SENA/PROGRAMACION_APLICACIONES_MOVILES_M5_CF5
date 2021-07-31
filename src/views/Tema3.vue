6055a62f723b9dcca011cd3b063a7829e9c2ad9f
luisEfe80 <luismendez80@hotmail.com>
2021-07-31 09:16:17 -0400

    Adicion de contenido

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -6,6 +6,40 @@
       .titulo-principal__numero
         span  3
       h1 Principales componentes de Playground   
+    .row.align-items-center.justify-content-center.mb-5
+      .col-lg-8
+        .bloque-texto-a.color-secundario.p-4.p-md-5
+          .row.m-0.align-items-center.justify-content-center
+            .col-lg-4.mb-4.mb-lg-0
+              figure
+                img.nW(src="@/assets/curso/t3-i1.svg", alt="imagen decorativa") 
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p <b>Playground</b> cuenta con unos elementos esenciales para el proceso de desarrollo de aplicaciones móviles híbridas; crea el nuevo proyecto y permite elegir el <b>framework</b> con el cual se desea desarrollar la aplicación.
+
+    TabsC.color-primario.mb-5
+      .py-3.py-md-4(titulo="The splendor of the mystery")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Celebrate your failures 1
+            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+        
+          .col-md-6
+            figure
+              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
+              figcaption Leyenda de la imagen
+      .py-3.py-md-4(titulo="Don’t lie to yourself")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            figure
+              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
+              figcaption Leyenda de la imagen
+          .col-md-6
+            h4 Celebrate your failures 2
+            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+
+
+
 
 
 
